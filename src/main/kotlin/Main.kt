ae04d141efd7225bfc37b873c458b4b617643990
Arthur <arturislamov97@gmail.com>
2022-03-27 20:08:23 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package module
+
+fun main() {
+        var firstName: String = "Arthur"
+        var lastName: String = "Islamov"
+        var height: Double = 175.0
+        var weight: Float = 90F
+        var isChild: Boolean = (height<150.0) || (weight<40)
+        var info: String = "Name: $firstName; Last name: $lastName; height: $height; weight: $weight; Is he child? - $isChild"
+        println(info)
+
+        height = 180.0
+        println(info)
+
+        // Дополнительные пунты "По желанию"
+        var isChild2: Boolean = (height>150.0) || (weight>40)
+        var info2: String = "Name: $firstName; Last name: $lastName; height: $height; weight: $weight; Is he child? - $isChild2"
+        println(info2)
+
+
+
+
+}
